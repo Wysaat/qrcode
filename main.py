4e43904239a7da29ecb0d1712154b9fc352d8c2b
Edmond <EdmondCarrson@gmail.com>
2013-05-12 10:36:27 -0700

    main file first edit

---
+++ main.py
@@ -0,0 +1,15 @@
+# step 1. data analysis
+
+# step 2. data encodation
+
+# step 3. error correction encoding
+
+# step 4. codeword generation
+
+# step 5. structure final message
+
+# step 6. module placement in the matrix
+
+# step 7. masking
+
+# step 8. format and version information
