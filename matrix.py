07ab2a3474e371d79b9d9380d1744e4f1c5ef075
Wysaat <Wysaat@gmail.com>
2013-05-14 12:03:12 -0700

    m

--- matrix.py
+++ matrix.py
@@ -3,3 +3,4 @@ from gendata import *
 getmatrix(version, finalmessage):
     size = matrixsizedict[version]
     remainderbits = remnumdict[version]
+    matrix = [[-1  for i in range(size)] for j in range(size)]
